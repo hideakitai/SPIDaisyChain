4dd09b17a7117845516f3bd62d365a474db940cb
Hideaki Tai <hideaki.tai@gmail.com>
2019-01-07 11:47:57 +0900

    fix build error

--- Examples/SPIDaisyChain/SPIDaisyChain.ino
+++ Examples/SPIDaisyChain/SPIDaisyChain.ino
@@ -6,7 +6,7 @@ const uint8_t NUM_DEVICE = 3;
 SPIDaisyChain spi(PIN_CS, NUM_DEVICE);	// CS pin, chained number
 
 byte send_data[NUM_DEVICE] = {1, 2, 3};
-byte recv_data[NUM_DEVICE] = {0, 0, 0};
+byte* recv_data;
 
 void setup()
 {
@@ -15,7 +15,7 @@ void setup()
 void loop()
 {
     // send data[0] to far device, data[1] to near device
-    recv_data = spi.transferDaisyChain(data);
+    recv_data = spi.transferDaisyChain(send_data);
 
     for (uint8_t i = 0; i < NUM_DEVICE; ++i)
     {
